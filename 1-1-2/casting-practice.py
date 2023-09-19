2037aa702a1eecad54d986d35b1a5cadcaf1bd29
eric_waller <eric.waller@vtsd.com>
2023-09-19 10:06:46 -0400

    Test File

---
+++ 1-1-2/casting-practice.py
@@ -0,0 +1,15 @@
+something = 1.9999999999999
+somethingelse = 946
+um="uh"
+
+print("Initial Values:")
+print("something", something)
+print("somethingelse", somethingelse)
+print("um:", um)
+
+somethingelse = chr(somethingelse)
+
+print("Ending Values:")
+print("something", something)
+print("somethingelse:", somethingelse)
+print("um:", um)
