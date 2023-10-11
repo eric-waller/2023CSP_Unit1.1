c47b655f1c2bd6fa58cb3ec4e4c81e5da7321e97
eric_waller <eric.waller@vtsd.com>
2023-10-11 08:31:28 -0400

    Test File

--- 1-1-6/step5.py
+++ 1-1-6/step5.py
@@ -7,16 +7,45 @@ spider.pensize(40)
 spider.circle(20)
 #configure spider legs
 leg_limit = 8
-leg_length = 70
+leg_length = 80
 angle = 360 / leg_limit
 spider.pensize(5)
 leg_num = 0
 #draw spider legs
 while (leg_num < leg_limit):
-  spider.goto(0, 20)
-  spider.setheading(angle * leg_num)
-  spider.forward(leg_length)
+  if (leg_num < 4):
+    spider.goto(0, 20)
+    spider.setheading(0.5 * angle * leg_num - 35)
+    spider.forward(leg_length)
+  else:
+    spider.goto(0, 20)
+    spider.setheading(90 + 0.5 * angle * leg_num - 35)
+    spider.forward(leg_length)
   leg_num = leg_num + 1
+
+spider.pencolor("purple")
+spider.penup()
+spider.goto(20, 0)
+spider.pensize(20)
+spider.pendown()
+spider.circle(10)
+
+spider.penup()
+spider.goto(-20, 0)
+spider.pendown()
+spider.circle(10)
+
+spider.pencolor("black")
+spider.penup()
+spider.goto(23, 1)
+spider.pendown()
+spider.circle(2)
+
+spider.penup()
+spider.goto(-19, 1)
+spider.pendown()
+spider.circle(2)
+
 spider.hideturtle()
 wn = trtl.Screen()
 wn.mainloop()
