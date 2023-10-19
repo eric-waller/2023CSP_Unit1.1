4d3bf931a49ab0783e7d7801272b384e176758df
eric_waller <eric.waller@vtsd.com>
2023-10-19 08:24:07 -0400

    Test File

--- 1-1-7/step9.py
+++ 1-1-7/step9.py
@@ -6,12 +6,19 @@ import turtle as trtl
 my_turtles = []
 
 # use interesting shapes and colors
-turtle_shapes = ["arrow", "turtle", "circle", "square", "triangle", "classic"]
-turtle_colors = ["red", "blue", "green", "orange", "purple", "gold"]
+turtle_shapes = ["arrow", "turtle", "circle", "square", "triangle", "classic", "arrow", "turtle", "circle", "square", "triangle", "classic"]
+turtle_colors = ["red", "blue", "green", "orange", "purple", "gold", "red", "blue", "green", "orange", "purple", "gold"]
 
 for s in turtle_shapes:
   t = trtl.Turtle(shape=s)
+  new_color = turtle_colors.pop()
+  t.pencolor(new_color)
+  t.fillcolor(new_color)
   my_turtles.append(t)
+
+direction = 90
+distance = 50
+size = 1
 
 #
 startx = 0
@@ -19,11 +26,18 @@ starty = 0
 
 #
 for t in my_turtles:
+  t.pensize(size)
   t.penup()
   t.goto(startx, starty)
   t.pendown()
+  t.setheading(direction)
   t.right(45)
-  t.forward(50)
+  t.forward(distance)
+  direction = t.heading()
+  distance = distance + 5
+  size = size + 1
+
+
 
 #
   startx = t.xcor()
