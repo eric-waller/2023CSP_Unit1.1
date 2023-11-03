54baa764035c7a1fb5e183d60ca5afbde05cd20b
eric_waller <eric.waller@vtsd.com>
2023-11-03 08:41:05 -0400

    Test File

--- 1-1-9/1.1.9.py
+++ 1-1-9/1.1.9.py
@@ -2,7 +2,9 @@ import turtle as trtl
 wn = trtl.Screen()
 
 shape = ["smallhim.gif"]
-him_gif = shape[0]
+order = 0
+him_gif = shape[order]
+
 wn.register_shape(him_gif)
 
 tyler = trtl.Turtle(shape = him_gif)
