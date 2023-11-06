1d0735ff43e997e1b5dd51bea2404c6912cdef5f
eric_waller <eric.waller@vtsd.com>
2023-11-06 07:56:44 -0500

    Test File

--- 1-1-9/1.1.9.py
+++ 1-1-9/1.1.9.py
@@ -1,14 +1,92 @@
 import turtle as trtl
+
+trtl1 = trtl.Turtle()
+
+trtles = [trtl, trtl1]
+
+trtl.speed(10)
+
+trtl1.speed(10)
+
+redvalue = 0
+
+greenvalue = 0
+
+bluevalue = 0
+
+wrmhle = []
+
+rad = 0.05
+
+fwd = 0.05
+
+numnum = ["i", "i", "i", "i", "i", "i", "i", "i", "i", "i"]
+
+for num in numnum:
+    wrmhle.append(slice)
+
+    wrmhle.append(slice)
+
+trtl.left(45)
+
+for sclice in wrmhle:
+    trtl.color = (redvalue, greenvalue, bluevalue)
+
+    trtl.circle(rad)
+
+    rad *= 1.5
+
+    redvalue += 5
+
+    greenvalue += 5
+
+    bluevalue += 5
+
+    trtl.penup()
+
+    trtl.forward(fwd)
+
+    trtl.pendown()
+
+    fwd *= 1.5
+
+redvalue = 0
+
+greenvalue = 0
+
+bluevalue = 0
+
+rad = 0.05
+
+fwd = 0.05
+
+trtl1.right(145)
+
+for sclice in wrmhle:
+    trtl1.color = (redvalue, greenvalue, bluevalue)
+
+    trtl1.circle(rad)
+
+    rad *= 1.5
+
+    redvalue += 5
+
+    greenvalue += 5
+
+    bluevalue += 5
+
+    trtl1.penup()
+
+    trtl1.forward(fwd)
+
+    trtl1.pendown()
+
+    fwd *= 1.5
+
+print(wrmhle)
+
+print(numnum)
+
 wn = trtl.Screen()
 
-shape = ["smallhim.gif"]
-order = 0
-him_gif = shape[order]
-
-wn.register_shape(him_gif)
-
-tyler = trtl.Turtle(shape = him_gif)
-
-tyler.forward(20)
-
-wn.mainloop()
+wn.mainloop
